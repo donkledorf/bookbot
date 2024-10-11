b4716062adc429026cf8000c2b94c3891bae94d0
donkledorf <stickeldr@gmail.com>
2024-10-11 03:01:28 -0700

    create character count

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     print(f"The book contains {word_count} words.")
+    character_count = count_characters(text)
+    print(character_count)
+    
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-#I need to split the entire text and then count each individual word once i have the number i need to 
-#return it as a string and print it
+
 def count_words(text):
    word = text.split()
    return len(word)
 
 
-    
+def count_characters(text):
+    characters = {}
+    for char in text.lower():
+        if char.isalpha():
+           characters[char] = characters.get(char, 0) + 1
+    return characters
+
+        
+
 
 main()
